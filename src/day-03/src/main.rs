31b0078cceb8d680b1a45756aec8c2e4a8f58df1
PraneethJain <praneethjain005@gmail.com>
2023-12-05 14:21:48 +0530

    feat: day 03 part 2

--- src/day-03/src/main.rs
+++ src/day-03/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
@@ -97,6 +98,88 @@ fn part_one(lines: &str) -> i32 {
     result
 }
 
+fn check_and_add(
+    grid: &Vec<Vec<char>>,
+    map: &mut HashMap<(usize, usize), Vec<i32>>,
+    x: usize,
+    y: usize,
+    num: i32,
+) {
+    if at(grid, x, y) == '*' {
+        if let Some(vector) = map.get_mut(&(x, y)) {
+            vector.push(num);
+        } else {
+            map.insert((x, y), vec![num]);
+        }
+    }
+}
+
+fn surrounded_with_check(
+    grid: &Vec<Vec<char>>,
+    x: usize,
+    y_start: usize,
+    y_end: usize,
+    map: &mut HashMap<(usize, usize), Vec<i32>>,
+) {
+    let num_str: String = grid[x][y_start..y_end].iter().collect();
+    let num = match num_str.parse::<i32>() {
+        Ok(res) => res,
+        Err(why) => panic!("Error parsing integer: {}", why),
+    };
+
+    let extremes = match y_start {
+        0 => vec![y_end],
+        y_start => vec![y_start - 1, y_end],
+    };
+
+    for y in extremes {
+        if x > 0 {
+            check_and_add(grid, map, x - 1, y, num);
+        }
+        check_and_add(grid, map, x, y, num);
+        check_and_add(grid, map, x + 1, y, num);
+    }
+
+    for y in y_start..y_end {
+        if x > 0 {
+            check_and_add(grid, map, x - 1, y, num);
+        }
+        check_and_add(grid, map, x + 1, y, num);
+    }
+}
+
+fn part_two(lines: &str) -> i32 {
+    let mut result = 0;
+    let mut map: HashMap<(usize, usize), Vec<i32>> = HashMap::new();
+
+    let grid = string_to_grid(lines);
+
+    for x in 0..grid.len() {
+        let mut y = 0;
+        while y < grid[x].len() {
+            let mut j = 0;
+            while y + j < grid[x].len() && grid[x][y + j].is_digit(10) {
+                j += 1;
+            }
+            y += match j {
+                0 => 1,
+                j => {
+                    surrounded_with_check(&grid, x, y, y + j, &mut map);
+                    j
+                }
+            }
+        }
+    }
+
+    for (_, nums) in map.iter() {
+        if nums.len() == 2 {
+            result += nums[0] * nums[1];
+        }
+    }
+
+    result
+}
+
 fn main() {
     let path = Path::new("input.txt");
     let display = path.display();
@@ -113,4 +196,5 @@ fn main() {
     }
 
     println!("{}", part_one(&lines));
+    println!("{}", part_two(&lines));
 }
