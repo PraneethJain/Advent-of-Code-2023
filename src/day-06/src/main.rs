28e9906808c1f16ba1f0a8fc6f289b19c2bf2f1a
PraneethJain <praneethjain005@gmail.com>
2023-12-06 14:48:47 +0530

    feat: day 06 part 1

---
+++ src/day-06/src/main.rs
@@ -0,0 +1,24 @@
+fn part_one(times: &[i64; 4], distances: &[i64; 4]) -> i64 {
+    let mut res: i64 = 1;
+    for (&t, &d) in times.iter().zip(distances) {
+        let i = ((t * t - 4 * d) as f64).sqrt();
+        let mut l = 0_f64.max((t as f64 - i) / 2.0).ceil() as i64;
+        let mut r = (d as f64).min((t as f64 + i) / 2.0).floor() as i64;
+        if l * (t - l) <= d {
+            l += 1;
+        }
+
+        if r * (t - r) <= d {
+            r -= 1;
+        }
+        res *= r - l + 1;
+    }
+    res
+}
+
+fn main() {
+    let times = [40, 82, 91, 66];
+    let distances = [277, 1338, 1349, 1063];
+
+    println!("{}", part_one(&times, &distances));
+}
