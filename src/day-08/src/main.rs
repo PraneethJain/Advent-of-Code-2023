80e76e9bc20ac27989430dc6f5fc95fc27de7663
PraneethJain <praneethjain005@gmail.com>
2023-12-09 14:26:52 +0530

    feat: day 08 part 2

--- src/day-08/src/main.rs
+++ src/day-08/src/main.rs
@@ -1,3 +1,4 @@
+use num_integer::lcm;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
@@ -10,7 +11,6 @@ fn part_one(lines: &str) -> i32 {
         None => panic!("input is empty"),
     };
 
-    println!("{}", directions);
     iter_lines.next();
     let mut adjacency_list: HashMap<String, (String, String)> = HashMap::new();
     while let Some(line) = iter_lines.next() {
@@ -50,6 +50,59 @@ fn part_one(lines: &str) -> i32 {
     res
 }
 
+fn part_two(lines: &str) -> i64 {
+    let mut iter_lines = lines.lines();
+    let directions = match iter_lines.next() {
+        Some(line) => line.trim(),
+        None => panic!("input is empty"),
+    };
+
+    iter_lines.next();
+    let mut adjacency_list: HashMap<String, (String, String)> = HashMap::new();
+    while let Some(line) = iter_lines.next() {
+        match line.split_once("=") {
+            Some((left, right)) => {
+                match right
+                    .replace("(", "")
+                    .replace(")", "")
+                    .replace(" ", "")
+                    .split_once(",")
+                {
+                    Some((l, r)) => {
+                        adjacency_list.insert(left.trim().to_owned(), (l.to_owned(), r.to_owned()));
+                    }
+                    None => panic!("couldn't find , in {}", right),
+                }
+            }
+            None => panic!("couldn't find = in {}", line),
+        }
+    }
+
+    adjacency_list
+        .keys()
+        .filter(|x| x.ends_with('A'))
+        .map(|position| {
+            let mut pos = position.to_owned();
+            let mut count = 0;
+            'outer: loop {
+                for dir in directions.chars() {
+                    let cur = &adjacency_list.get(&pos).unwrap();
+                    match dir {
+                        'L' => pos = cur.0.to_owned(),
+                        'R' => pos = cur.1.to_owned(),
+                        _ => panic!("invalid direction {}", dir),
+                    }
+                    count += 1;
+                    if pos.ends_with('Z') {
+                        break 'outer;
+                    }
+                }
+            }
+            count
+        })
+        .fold(1, |acc, x| lcm(acc, x))
+}
+
 fn main() {
     let path = Path::new("input.txt");
     let display = path.display();
@@ -66,4 +119,5 @@ fn main() {
     }
 
     println!("{}", part_one(&lines));
+    println!("{}", part_two(&lines));
 }
