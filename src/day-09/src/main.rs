51115adce35aa349cc134f7e4b4a87bebd0fed4d
PraneethJain <praneethjain005@gmail.com>
2023-12-09 14:55:37 +0530

    feat: day 09 part 2

--- src/day-09/src/main.rs
+++ src/day-09/src/main.rs
@@ -37,6 +37,41 @@ fn part_one(lines: &str) -> i32 {
     res
 }
 
+fn part_two(lines: &str) -> i32 {
+    let mut res = 0;
+
+    for line in lines.lines() {
+        let nums: Vec<_> = line
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .rev()
+            .collect();
+        let mut num_of_nums = vec![nums];
+
+        while !num_of_nums.last().unwrap().iter().all(|&x| x == 0) {
+            num_of_nums.push(
+                num_of_nums
+                    .last()
+                    .unwrap()
+                    .windows(2)
+                    .map(|pair| pair[1] - pair[0])
+                    .collect(),
+            )
+        }
+
+        loop {
+            let last = num_of_nums.pop().unwrap().pop().unwrap();
+            if num_of_nums.is_empty() {
+                res += last;
+                break;
+            } else {
+                *num_of_nums.last_mut().unwrap().last_mut().unwrap() += last;
+            }
+        }
+    }
+
+    res
+}
 fn main() {
     let path = Path::new("input.txt");
     let display = path.display();
@@ -53,4 +88,5 @@ fn main() {
     }
 
     println!("{}", part_one(&lines));
+    println!("{}", part_two(&lines));
 }
