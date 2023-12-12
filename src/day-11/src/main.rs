22f342d89252684e2557a1b0acd2278d5be5a927
PraneethJain <praneethjain005@gmail.com>
2023-12-12 21:36:38 +0530

    feat: day 11 part 2

--- src/day-11/src/main.rs
+++ src/day-11/src/main.rs
@@ -64,6 +64,57 @@ fn part_one(lines: &str) -> i32 {
     res
 }
 
+fn part_two(lines: &str) -> i64 {
+    let grid = string_to_grid(lines);
+    let empty_rows: BTreeSet<_> = grid
+        .iter()
+        .enumerate()
+        .filter_map(|(x, vec)| {
+            if vec.iter().all(|&c| c == '.') {
+                Some(x)
+            } else {
+                None
+            }
+        })
+        .collect();
+    let empty_columns: BTreeSet<_> = (0..grid[0].len())
+        .filter_map(|y| {
+            if grid.iter().all(|vec| vec[y] == '.') {
+                Some(y)
+            } else {
+                None
+            }
+        })
+        .collect();
+    let galaxy_positions: BTreeSet<_> = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(x, vec)| {
+            vec.iter()
+                .enumerate()
+                .filter_map(move |(y, &c)| if c == '#' { Some((x, y)) } else { None })
+        })
+        .collect();
+
+    let mut res = 0;
+    for (i, &(x1, y1)) in galaxy_positions.iter().enumerate() {
+        for &(x2, y2) in galaxy_positions.iter().skip(i + 1) {
+            let (startx, endx) = (x1.min(x2), x1.max(x2));
+            let (starty, endy) = (y1.min(y2), y1.max(y2));
+            for x in startx..endx {
+                res += if empty_rows.contains(&x) { 1000000 } else { 1 }
+            }
+            for y in starty..endy {
+                res += if empty_columns.contains(&y) {
+                    1000000
+                } else {
+                    1
+                }
+            }
+        }
+    }
+    res
+}
 fn main() {
     let path = Path::new("input.txt");
 
@@ -72,4 +123,5 @@ fn main() {
     file.read_to_string(&mut lines).expect("couldn't read file");
 
     println!("{}", part_one(&lines));
+    println!("{}", part_two(&lines));
 }
