414e7f06ff62da4521c69c223a9cad79f7a83843
PraneethJain <praneethjain005@gmail.com>
2023-12-16 14:58:45 +0530

    feat: day 15 part 2

--- src/day-15/src/main.rs
+++ src/day-15/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
@@ -17,10 +18,42 @@ fn part_one(lines: &str) -> usize {
         .split(',')
         .map(|x| {
             let y = hash_str(x.trim());
-            println!("{}", y);
             y
         })
         .sum()
+}
+
+fn part_two(lines: &str) -> usize {
+    let mut boxes: Vec<Vec<&str>> = vec![vec![]; 256];
+    let mut focal_lengths: HashMap<&str, usize> = HashMap::new();
+    for line in lines.split(',') {
+        if line.contains('=') {
+            let (name, focal_length) = line.trim().split_once("=").expect("no = in input");
+            focal_lengths.insert(
+                name,
+                focal_length
+                    .parse::<usize>()
+                    .expect("couldn't parse to usize"),
+            );
+
+            let box_number = hash_str(name);
+            if !boxes[box_number].contains(&name) {
+                boxes[box_number].push(name);
+            }
+        } else {
+            let (name, _) = line.split_once('-').expect("no - in input");
+            let box_number = hash_str(name);
+            boxes[box_number].retain(|&x| x != name);
+        }
+    }
+
+    boxes.iter().enumerate().fold(0, |acc, (box_num, boxx)| {
+        acc + boxx.iter().enumerate().fold(0, |ac, (slot_num, name)| {
+            ac + (box_num + 1)
+                * (slot_num + 1)
+                * focal_lengths.get(name).expect("focal length not found")
+        })
+    })
 }
 
 fn main() {
@@ -39,4 +72,5 @@ fn main() {
     }
 
     println!("{}", part_one(&lines));
+    println!("{}", part_two(&lines));
 }
