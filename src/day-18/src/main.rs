668a9eb0dd6f035a50555ca25227cca4cb3d7a6e
PraneethJain <praneethjain005@gmail.com>
2023-12-18 22:03:24 +0530

    feat: day 18 part 2

--- src/day-18/src/main.rs
+++ src/day-18/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-fn shoelace(points: &Vec<(i32, i32)>) -> i32 {
+fn shoelace(points: &Vec<(i64, i64)>) -> i64 {
     points
         .windows(2)
         .fold(0, |acc, ps| acc + ps[0].0 * ps[1].1 - ps[0].1 * ps[1].0)
@@ -10,19 +10,47 @@ fn shoelace(points: &Vec<(i32, i32)>) -> i32 {
         / 2
 }
 
-fn part_one(lines: &str) -> i32 {
+fn part_one(lines: &str) -> i64 {
     let mut cur = (0, 0);
     let mut points = vec![(0, 0)];
-    let mut p = 0;
+    let mut p: i64 = 0;
     for line in lines.lines() {
         let parts: Vec<_> = line.split_whitespace().map(|x| x.trim()).collect();
-        let num: i32 = parts[1].parse().expect("couldn't parse to i32");
+        let num: i64 = parts[1].parse().expect("couldn't parse to i64");
         p += num;
         match parts[0] {
             "U" => cur.1 += num,
             "R" => cur.0 += num,
             "D" => cur.1 -= num,
             "L" => cur.0 -= num,
+            other => panic!("found {}", other),
+        };
+        points.push(cur.clone());
+    }
+    // Pick's Theorem
+    // area = i + p/2 - 1, where i is number of points inside, p is number of points on
+    let area = shoelace(&points);
+    let i = area - p / 2 + 1;
+    i + p
+}
+
+fn part_two(lines: &str) -> i64 {
+    let mut cur = (0, 0);
+    let mut points = vec![(0, 0)];
+    let mut p: i64 = 0;
+    for line in lines.lines() {
+        let hex = line
+            .split_whitespace()
+            .map(|x| x.trim())
+            .last()
+            .expect("string splits into empty iterator");
+        let num = i64::from_str_radix(&hex[2..7], 16).expect("couldn't read as hex");
+        p += num;
+        match &hex[7..8] {
+            "3" => cur.1 += num,
+            "0" => cur.0 += num,
+            "1" => cur.1 -= num,
+            "2" => cur.0 -= num,
             other => panic!("found {}", other),
         };
         points.push(cur.clone());
@@ -50,4 +78,5 @@ fn main() {
     }
 
     println!("{}", part_one(&lines));
+    println!("{}", part_two(&lines));
 }
