20539c8fb05609b99055aa1902b89427159cf6eb
PraneethJain <praneethjain005@gmail.com>
2023-12-21 20:51:34 +0530

    feat: day 20 part 2

--- src/day-20/src/main.rs
+++ src/day-20/src/main.rs
@@ -102,6 +102,106 @@ fn part_one(lines: &str) -> i32 {
     total_low * total_high
 }
 
+fn button_press_till_rx(
+    modules: &mut BTreeMap<String, Module>,
+    q: &VecDeque<(String, String, bool)>,
+    cycles: &mut BTreeMap<String, i64>,
+    count: i64,
+) -> bool {
+    let mut q = q.clone();
+    while !q.is_empty() {
+        let (source, cur, strength) = q.pop_front().expect("queue is empty");
+
+        // hardcoded from input
+        if cur == "lx" && strength {
+            if !cycles.contains_key(&source) {
+                cycles.insert(source.clone(), count);
+            }
+        }
+        // hardcoded from input
+        if cycles.len() == 4 {
+            return true;
+        }
+
+        match modules.get_mut(&cur) {
+            Some(Module::FlipFlop(state, destinations)) => {
+                if strength == false {
+                    *state = !*state;
+                    for destination in destinations {
+                        q.push_back((cur.to_string(), destination.to_string(), *state));
+                    }
+                }
+            }
+            Some(Module::Conjunction(state, destinations)) => {
+                *state.get_mut(&source).expect("source not initialized") = strength;
+                let pulse = !state.values().all(|&x| x);
+                for destination in destinations {
+                    q.push_back((cur.to_string(), destination.to_string(), pulse));
+                }
+            }
+            None => (),
+        }
+    }
+    false
+}
+
+fn part_two(lines: &str) -> i64 {
+    let mut modules: BTreeMap<String, Module> = BTreeMap::new();
+    let mut q: VecDeque<(String, String, bool)> = VecDeque::new();
+    for line in lines.lines() {
+        let (source, destinations) = match line.split_once("->") {
+            Some((from, tos)) => (from.trim(), tos.trim()),
+            None => panic!("no -> in line {}", line),
+        };
+        let vec: Vec<_> = destinations
+            .trim()
+            .split(',')
+            .map(|x| x.trim().to_string())
+            .collect();
+
+        if source.starts_with('%') {
+            modules.insert(source[1..].to_string(), Module::FlipFlop(false, vec));
+        } else if source.starts_with('&') {
+            modules.insert(
+                source[1..].to_string(),
+                Module::Conjunction(BTreeMap::new(), vec),
+            );
+        } else if source == "broadcaster" {
+            for destination in vec {
+                q.push_back((source.to_string(), destination.to_string(), false));
+            }
+        } else {
+            panic!("found {} in from", source);
+        }
+    }
+
+    for (x, y) in modules.clone() {
+        let destinations = match y {
+            Module::FlipFlop(_, destinations) => destinations,
+            Module::Conjunction(_, destinations) => destinations,
+        };
+
+        for destination in destinations {
+            match modules.get_mut(&destination) {
+                Some(module) => match module {
+                    Module::FlipFlop(_, _) => (),
+                    Module::Conjunction(state, _) => {
+                        state.insert(x.to_string(), false);
+                    }
+                },
+                None => (),
+            }
+        }
+    }
+
+    let mut cycles: BTreeMap<String, i64> = BTreeMap::new();
+    let mut count = 1;
+    while !button_press_till_rx(&mut modules, &q, &mut cycles, count) {
+        count += 1;
+    }
+    cycles.values().product()
+}
+
 fn main() {
     let path = Path::new("input.txt");
     let display = path.display();
@@ -118,4 +218,5 @@ fn main() {
     }
 
     println!("{}", part_one(&lines));
+    println!("{}", part_two(&lines));
 }
