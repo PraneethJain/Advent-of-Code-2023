e6c2fa93347dd278dd8273156c5af0c75a877680
PraneethJain <praneethjain005@gmail.com>
2023-12-24 15:17:57 +0530

    feat: day 23 part 2

--- src/day-23/src/main.rs
+++ src/day-23/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
@@ -57,6 +58,44 @@ fn part_one(lines: &str) -> i32 {
     dfs(&grid, (0, 1), (0, 0))
 }
 
+fn dfs_seen(
+    cur: (i32, i32),
+    end: (i32, i32),
+    grid: &Vec<Vec<char>>,
+    seen: &mut HashSet<(i32, i32)>,
+) -> (i32, bool) {
+    let valid = |pos: (i32, i32)| -> bool {
+        pos.0 >= 0 && pos.0 < grid.len() as i32 && pos.1 >= 0 && pos.1 < grid[0].len() as i32
+    };
+    let mut res = 1;
+    let mut reached_end = false;
+    for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+        let (nx, ny) = (cur.0 + dx, cur.1 + dy);
+        if !seen.contains(&(nx, ny)) && valid((nx, ny)) && grid[nx as usize][ny as usize] != '#' {
+            if (nx, ny) == end {
+                reached_end = true;
+            }
+            seen.insert((nx, ny));
+            let (count, reached_end_sub) = dfs_seen((nx, ny), end, grid, seen);
+            if reached_end_sub {
+                res = res.max(1 + count);
+                reached_end = true;
+            }
+            seen.remove(&(nx, ny));
+        }
+    }
+    (res, reached_end)
+}
+
+fn part_two(lines: &str) -> i32 {
+    let grid = string_to_grid(lines);
+    let mut seen: HashSet<(i32, i32)> = HashSet::new();
+    let start = (0, 1);
+    let end = (grid.len() as i32 - 1, grid[0].len() as i32 - 2);
+    seen.insert(start);
+    dfs_seen(start, end, &grid, &mut seen).0
+}
+
 fn main() {
     let path = Path::new("input.txt");
     let display = path.display();
@@ -73,4 +112,5 @@ fn main() {
     }
 
     println!("{}", part_one(&lines));
+    println!("{}", part_two(&lines)); // 20 minutes 48 seconds
 }
