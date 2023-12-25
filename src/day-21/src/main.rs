cecc7de6f343a1d1a9a4d4b751cda77ccd1a45c5
PraneethJain <praneethjain005@gmail.com>
2023-12-25 23:04:48 +0530

    feat: day 21 part 2

--- src/day-21/src/main.rs
+++ src/day-21/src/main.rs
@@ -71,6 +71,50 @@ fn part_one(lines: &str) -> i32 {
     res
 }
 
+fn count(grid: &[Vec<char>]) -> (i64, i64, i64) {
+    let start = find_start(grid).expect("no start found");
+    let mut q = BTreeSet::from([start]);
+
+    let at =
+        |x: i32, y: i32| -> char { grid[x.rem_euclid(131) as usize][y.rem_euclid(131) as usize] };
+
+    let mut res = Vec::new();
+    let mut i = 0;
+    loop {
+        if i % 131 == 65 {
+            res.push(q.len() as i64);
+            if res.len() == 3 {
+                break (res[0], res[1], res[2]);
+            }
+        }
+
+        let mut new_q = BTreeSet::new();
+        for (x, y) in q {
+            for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+                let (xx, yy) = (x + dx, y + dy);
+                // can be 'S' too!!
+                if at(xx, yy) != '#' {
+                    new_q.insert((xx, yy));
+                }
+            }
+        }
+        q = new_q;
+        i += 1;
+    }
+}
+
+fn part_two(lines: &str) -> i64 {
+    let grid = string_to_grid(lines);
+    // f(x) = c1 x^2 + c2 x + c3
+    // f(0), f(1), f(2)
+    let (a, b, c) = count(&grid);
+    let c3 = a;
+    let c1 = (c - 2 * b + a) / 2;
+    let c2 = b - c1 - c3;
+    let f = |x: i64| -> i64 { c1 * x * x + c2 * x + c3 };
+    f(202300)
+}
+
 fn main() {
     let path = Path::new("input.txt");
     let display = path.display();
@@ -86,4 +130,5 @@ fn main() {
     }
 
     println!("{}", part_one(&lines));
+    println!("{}", part_two(&lines)); // 2s on release mode
 }
